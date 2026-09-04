96dbcb0de348e2ce45665a60765208ee78a1d6f6
kazuo fujimoto <kazuo.fujimoto2007@gmail.com>
2026-09-04 21:30:58 +0900

    再度、変更

--- R/app_ui.R
+++ R/app_ui.R
@@ -9,7 +9,11 @@ app_ui <- function(app_version = "0.95") {
       h2("多重対応分析MCA分析サポート", paste("Version:", app_version)),
       # パッケージ名が ShinyspeMCA の場合
 #      includeMarkdown(system.file("www/overview.md", package = "ShinyspeMCA")),
-      HTML(markdown::mark_html(system.file("www/overview.md", package = "ShinyspeMCA"))),
+#      HTML(markdown::mark_html(system.file("www/overview.md", package = "ShinyspeMCA"))),
+       HTML(markdown::mark_html(
+         text = paste(readLines(system.file("www/overview.md", package = "ShinyspeMCA"),
+                                encoding = "UTF-8"), collapse = "\n")
+         )),
       hr(),
       h3("Powered by & Supported by"),
       div(
